0d7c67863b275fe83874f18050fedfeec1c2a5bf
Robin Raju <robinraju@users.noreply.github.com>
2022-04-03 15:14:08 +0200

    Modify recursive crawling logic

--- src/main/scala/com/robinraju/crawler/CrawlManager.scala
+++ src/main/scala/com/robinraju/crawler/CrawlManager.scala
@@ -1,14 +1,13 @@
 package com.robinraju.crawler
 
-import java.net.URL
-
 import akka.actor.typed.scaladsl.{ ActorContext, Behaviors }
 import akka.actor.typed.{ ActorRef, Behavior }
-
 import com.robinraju.cache.WebCrawlerCache
 import com.robinraju.core.CrawledPageResult
 import com.robinraju.io.TSVWriter
 import com.robinraju.util.DomainRatioUtil
+
+import java.net.URL
 
 object CrawlManager {
 
@@ -25,7 +24,7 @@ object CrawlManager {
           LinkHarvester(harvesterResponseMapper, cache),
           "link-harvester"
         )
-      new CrawlManager(linkHarvester, tsvWriter, context).crawling(0, maxDepth)
+      new CrawlManager(linkHarvester, tsvWriter, context).crawling(0, maxDepth, Map())
     }
 }
 
@@ -36,29 +35,42 @@ class CrawlManager private (
 ) {
   import CrawlManager._
 
-  def crawling(currentDepth: Int, maxDepth: Int): Behavior[ManagerCommand] = Behaviors.receiveMessage {
-    case StartCrawling(seedUrl) =>
-      val depth = currentDepth + 1
-      linkHarvester ! LinkHarvester.HarvestLinks(Set(seedUrl), depth)
-      crawling(depth, maxDepth)
+  def crawling(currentDepth: Int, maxDepth: Int, queuedRequests: Map[Int, Int]): Behavior[ManagerCommand] =
+    Behaviors.receiveMessage {
+      case StartCrawling(seedUrl) =>
+        val depth = currentDepth + 1
+        linkHarvester ! LinkHarvester.HarvestLinks(Set(seedUrl), depth)
+        crawling(depth, maxDepth, Map(depth -> 1))
 
-    case HarvesterResponseWrapper(response) =>
-      response match {
-        case LinkHarvester.HarvestedLinks(parentUrl, childUrls, previousDepth) =>
-          val sameDomainRatio = DomainRatioUtil.calculateSameDomainRatio(parentUrl, childUrls)
+      case HarvesterResponseWrapper(response) =>
+        response match {
+          case LinkHarvester.HarvestedLinks(parentUrl, childUrls, depth) =>
+            val sameDomainRatio = DomainRatioUtil.calculateSameDomainRatio(parentUrl, childUrls)
 
-          context.log.info(s"URL: $parentUrl  Depth: $currentDepth  Ratio: $sameDomainRatio")
-          context.log.info(s"<< Current Depth: $currentDepth Max: $maxDepth")
-          tsvWriter ! TSVWriter.WriteToFile(CrawledPageResult(parentUrl.toString, currentDepth, sameDomainRatio))
-          if (currentDepth == maxDepth) {
-            Behaviors.same
-          } else {
-            linkHarvester ! LinkHarvester.HarvestLinks(childUrls, previousDepth + 1)
-            if (currentDepth == previousDepth)
+            context.log.info(s"URL: $parentUrl  Depth: $depth  Ratio: $sameDomainRatio")
+            context.log.info(s"Current Depth: $currentDepth Max: $maxDepth")
+            tsvWriter ! TSVWriter.WriteToFile(CrawledPageResult(parentUrl.toString, depth, sameDomainRatio))
+
+            val requestsInFlight = queuedRequests.updated(depth, Math.max(queuedRequests(depth) - 1, 0))
+
+            if (currentDepth == maxDepth) {
               Behaviors.same
-            else
-              crawling(currentDepth + 1, maxDepth)
-          }
-      }
-  }
+            } else {
+              if (requestsInFlight(currentDepth) == 0) {
+                linkHarvester ! LinkHarvester.HarvestLinks(childUrls, depth + 1)
+                crawling(
+                  currentDepth + 1,
+                  maxDepth,
+                  requestsInFlight.removed(currentDepth).updated(currentDepth + 1, childUrls.size)
+                )
+              } else {
+                linkHarvester ! LinkHarvester.HarvestLinks(childUrls, depth + 1)
+                crawling(currentDepth, maxDepth, requestsInFlight.updated(depth + 1, childUrls.size))
+              }
+            }
+          case LinkHarvester.LinkHarvestFailed(depth) =>
+            val updatedRequests = queuedRequests.updated(depth, Math.max(queuedRequests(depth) - 1, 0))
+            crawling(currentDepth, maxDepth, updatedRequests)
+        }
+    }
 }
